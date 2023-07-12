09d13909264fd5aafe2032271a915a4eae75f8ed
stgoneira <stgoneiran@gmail.com>
2023-07-11 23:52:56 -0400

    ListView simple_list_item_1 y simple_list_item_2 example

---
+++ app/src/main/java/cl/stgoneira/learning/android/listas/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.stgoneira.learning.android.listas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
